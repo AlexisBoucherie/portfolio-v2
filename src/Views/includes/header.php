dcc23ff7ea62f856bcf671f07c5702eb03ac4fff
AlexisBoucherie <alexis.boucherie@gmail.com>
2022-10-30 09:44:38 +0100

    changed nav path for testing

--- src/Views/includes/header.php
+++ src/Views/includes/header.php
@@ -22,10 +22,10 @@
     <div id="mySidenav" class="sidenav">
         <i id="closeBtn" class="bi bi-x-circle close"></i>
         <ul id="closeAction">
-            <li><a href="home"><i class="bi bi-house"></i> Accueil</a></li>
-            <li><a href="about"><i class="bi bi-file-earmark-person"></i> À propos</a></li>
-            <li><a href="achievements"><i class="bi bi-card-checklist"></i> Réalisations</a></li>
-            <li><a href="contacts"><i class="bi bi-envelope"></i> Contacts</a></li>
+            <li><a href="/home"><i class="bi bi-house"></i> Accueil</a></li>
+            <li><a href="/about"><i class="bi bi-file-earmark-person"></i> À propos</a></li>
+            <li><a href="/achievements"><i class="bi bi-card-checklist"></i> Réalisations</a></li>
+            <li><a href="/contacts"><i class="bi bi-envelope"></i> Contacts</a></li>
         </ul>
     </div>
 
@@ -38,10 +38,10 @@
     <!-- nav tablette et desktop -->
     <nav class="highres-nav">
         <ul>
-            <li><a href="home">Accueil</a></li>
-            <li><a href="about">À propos</a></li>
-            <li><a href="achievements">Réalisations</a></li>
-            <li><a href="contacts">Contacts</a></li>
+            <li><a href="/home">Accueil</a></li>
+            <li><a href="/about">À propos</a></li>
+            <li><a href="/achievements">Réalisations</a></li>
+            <li><a href="/contacts">Contacts</a></li>
         </ul>
     </nav>
 </header>
