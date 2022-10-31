6158c49dbe3b33dcd785a02fd931e0a4d3f52285
AlexisBoucherie <alexis.boucherie@gmail.com>
2022-10-31 11:19:09 +0100

    better css for all resolutions

--- src/Views/contacts.php
+++ src/Views/contacts.php
@@ -4,17 +4,19 @@
 <!-- PAGE CONTENT -->
 <main>
     <div class="contacts" id="contacts">
-
-        <h3>Contactez-moi :</h3>
-        <p><i class="bi bi-telephone"></i> | Par téléphone - <a href="tel:+33674325187">+33(0)6.74.32.51.87</a></p>
-        <p><i class="bi bi-envelope"></i> | Par mail - <a href="mailto:alexis.boucherie@gmail.com">alexis.boucherie@gmail.com</a>
-        </p>
-
-        <h3>Retrouvez-moi :</h3>
-        <p><i class="bi bi-linkedin"></i> | Sur Linkedin - <a href="https://www.linkedin.com/in/alexis-boucherie/">linkedin.com/in/alexis-boucherie</a>
-        </p>
-        <p><i class="bi bi-github"></i> | Sur Github - <a href="https://github.com/AlexisBoucherie">github.com/AlexisBoucherie</a>
-        </p>
+        <div class="contact-me">
+            <h3>Contactez-moi :</h3>
+            <p><i class="bi bi-telephone"></i> | Par téléphone - <a href="tel:+33674325187">+33(0)6.74.32.51.87</a></p>
+            <p><i class="bi bi-envelope"></i> | Par mail - <a href="mailto:alexis.boucherie@gmail.com">alexis.boucherie@gmail.com</a>
+            </p>
+        </div>
+        <div class="find-me">
+            <h3>Retrouvez-moi :</h3>
+            <p><i class="bi bi-linkedin"></i> | Sur Linkedin - <a href="https://www.linkedin.com/in/alexis-boucherie/">linkedin.com/in/alexis-boucherie</a>
+            </p>
+            <p><i class="bi bi-github"></i> | Sur Github - <a href="https://github.com/AlexisBoucherie">github.com/AlexisBoucherie</a>
+            </p>
+        </div>
 
     </div>
 </main>
